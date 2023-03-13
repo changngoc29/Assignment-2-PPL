bd5de843eeffe671cf63c2193050e8a535e7e02c
vtkienn <vtkienn@gmail.com>
2023-03-13 16:45:15 +0700

    13/3 commit 2

--- src/main/mt22/astgen/ASTGeneration.py
+++ src/main/mt22/astgen/ASTGeneration.py
@@ -23,7 +23,7 @@ class ASTGeneration(MT22Visitor):
     # vardecl
     def visitVardecl(self, ctx: MT22Parser.VardeclContext):
         # print('visitVardecl')
-        return self.visit(ctx.vardeclnoinit()) if ctx.vardeclnoinit() else self.visit(ctx.vardeclinit)
+        return self.visit(ctx.vardeclnoinit()) if ctx.vardeclnoinit() else self.visit(ctx.vardeclinit())
 
     # vardeclnoinit
     def visitVardeclnoinit(self, ctx: MT22Parser.VardeclnoinitContext):
@@ -31,8 +31,27 @@ class ASTGeneration(MT22Visitor):
         return [VarDecl(x, self.visit(ctx.typ())) for x in self.visit(ctx.idlist())]
 
     # vardeclinit
+    arrayIdVarDeclInit = []
+    arrayExprVarDeclInit = []
+
     def visitVardeclinit(self, ctx: MT22Parser.VardeclinitContext):
-        return self.visitChildren(ctx)
+        # print('visitVardeclinit')
+        if ctx.typ():
+            self.arrayIdVarDeclInit += [ctx.ID().getText()]
+            self.arrayExprVarDeclInit += [self.visit(ctx.expr())]
+
+            arrayIdVarDeclInit = self.arrayIdVarDeclInit
+            arrayExprVarDeclInit = self.arrayExprVarDeclInit[::-1]
+
+            assocArray = list(zip(arrayIdVarDeclInit, arrayExprVarDeclInit))
+
+            self.arrayIdVarDeclInit.clear()
+            self.arrayExprVarDeclInit.clear()
+
+            return [VarDecl(x[0], self.visit(ctx.typ()), x[1]) for x in assocArray]
+        self.arrayIdVarDeclInit += [ctx.ID().getText()]
+        self.arrayExprVarDeclInit += [self.visit(ctx.expr())]
+        return self.visit(ctx.vardeclinit())
 
     def visitFuncdecl(self, ctx: MT22Parser.FuncdeclContext):
         return self.visitChildren(ctx)
@@ -49,44 +68,109 @@ class ASTGeneration(MT22Visitor):
     def visitFunctyp(self, ctx: MT22Parser.FunctypContext):
         return self.visitChildren(ctx)
 
+    # EXPRESSION
+    # expr
     def visitExpr(self, ctx: MT22Parser.ExprContext):
-        return self.visitChildren(ctx)
+        print('visitExpr')
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.relationalExpr(0))
+        return BinExpr(ctx.CONCATOP().getText(), self.visit(ctx.relationalExpr(0)), self.visit(ctx.relationalExpr(1)))
 
+    # relationalExpr
     def visitRelationalExpr(self, ctx: MT22Parser.RelationalExprContext):
-        return self.visitChildren(ctx)
+        print('visitRelationalExpr')
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.logicalExpr(0))
+        return BinExpr(self.visit(ctx.relationalOpt()), self.visit(ctx.logicalExpr(0)), self.visit(ctx.logicalExpr(1)))
 
+    # relationalOpt
     def visitRelationalOpt(self, ctx: MT22Parser.RelationalOptContext):
-        return self.visitChildren(ctx)
+        print('visitRelationalOpt')
+        if ctx.EQCOP():
+            return ctx.EQCOP().getText()
+        elif ctx.NOTEQOP():
+            return ctx.NOTEQOP().getText()
+        elif ctx.SMOP():
+            return ctx.SMOP().getText()
+        elif ctx.GTOP():
+            return ctx.GTOP().getText()
+        elif ctx.SMEOP():
+            return ctx.SMEOP().getText()
+        return ctx.GTEOP().getText()
 
+    # logicalExpr
     def visitLogicalExpr(self, ctx: MT22Parser.LogicalExprContext):
-        return self.visitChildren(ctx)
+        print('visitLogicalExpr')
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.addExpr())
+        return BinExpr(self.visit(ctx.logicalOpt()), self.visit(ctx.logicalExpr()), self.visit(ctx.addExpr()))
 
+    # logicalOpt
     def visitLogicalOpt(self, ctx: MT22Parser.LogicalOptContext):
-        return self.visitChildren(ctx)
+        print('visitLogicalOpt')
+        if ctx.ANDOP():
+            return ctx.ANDOP().getText()
+        return ctx.OROP.getText()
 
+    # addExpr
     def visitAddExpr(self, ctx: MT22Parser.AddExprContext):
-        return self.visitChildren(ctx)
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.multiExpr())
+        return BinExpr(self.visit(ctx.addOpt()), self.visit(ctx.addExpr()), self.visit(ctx.multiExpr()))
 
+    # addOpt
     def visitAddOpt(self, ctx: MT22Parser.AddOptContext):
-        return self.visitChildren(ctx)
+        print('visitAddOpt')
+        return ctx.ADDOP().getText() if ctx.ADDOP() else ctx.SUBOP().getText()
 
+    # multiExpr
     def visitMultiExpr(self, ctx: MT22Parser.MultiExprContext):
-        return self.visitChildren(ctx)
+        print('visistMultiExpr')
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.unaryLogicalExpr())
+        return BinExpr(self.visit(ctx.multiOpt()), self.visit(ctx.multiExpr()), self.visit(ctx.unaryLogicalExpr()))
 
+    # multiOpt
     def visitMultiOpt(self, ctx: MT22Parser.MultiOptContext):
-        return self.visitChildren(ctx)
+        print('visitMultiOpt')
+        if ctx.MULOP():
+            return ctx.MULOP().getText()
+        elif ctx.DIVOP():
+            return ctx.DIVOP().getText()
+        return ctx.MODOP().getText()
 
+    # logicalExpr
     def visitUnaryLogicalExpr(self, ctx: MT22Parser.UnaryLogicalExprContext):
-        return self.visitChildren(ctx)
+        print('visitUnaryLogicalExpr')
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.signExpr())
+        return UnExpr(ctx.NOTOP().getText(), self.visit(ctx.unaryLogicalExpr()))
 
+    # signExpr
     def visitSignExpr(self, ctx: MT22Parser.SignExprContext):
-        return self.visitChildren(ctx)
+        print('visitSignExpr')
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.indexOptExpr())
+        return UnExpr(ctx.SUBOP().getText(), self.visit(ctx.signExpr()))
 
+    # indexOptExpr
     def visitIndexOptExpr(self, ctx: MT22Parser.IndexOptExprContext):
-        return self.visitChildren(ctx)
+        print('visitIndexOptExpr')
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.subexpr())
+        # ID LS nonnullexprlist RS
+        return None
 
+    # subexpr
     def visitSubexpr(self, ctx: MT22Parser.SubexprContext):
-        return self.visitChildren(ctx)
+        print('visitSubexpr')
+        if ctx.ID():
+            return Id(ctx.ID().getText())
+        elif ctx.alllit():
+            return self.visit(ctx.alllit())
+        elif ctx.expr():
+            return self.visit(ctx.expr())
+        return self.visit(ctx.callexpr())
 
     def visitCallexpr(self, ctx: MT22Parser.CallexprContext):
         return self.visitChildren(ctx)
@@ -193,8 +277,18 @@ class ASTGeneration(MT22Visitor):
             return int(ctx.INTLIT().getText())
         return self.visit(ctx.expr())
 
+    # alllit: INTLIT | STRINGLIT | FLOATLIT | TRUE | FALSE | arrayLit;
     def visitAlllit(self, ctx: MT22Parser.AlllitContext):
-        return self.visitChildren(ctx)
+        print('visitAlllit')
+        if ctx.INTLIT():
+            return IntegerLit(int(ctx.INTLIT().getText()))
+        elif ctx.STRINGLIT():
+            return StringLit(ctx.STRINGLIT().getText())
+        elif ctx.FLOATLIT():
+            return FloatLit(float(ctx.FLOATLIT().getText()))
+        elif ctx.TRUE() or ctx.FALSE():
+            return BooleanLit(True if ctx.TRUE() else False)
+        return self.visit(ctx.arrayLit())
 
     def visitArrayLit(self, ctx: MT22Parser.ArrayLitContext):
         return self.visitChildren(ctx)
--- src/test/ASTGenSuite.py
+++ src/test/ASTGenSuite.py
@@ -10,26 +10,26 @@ class ASTGenSuite(unittest.TestCase):
         # print(expect)
         self.assertTrue(TestAST.test(input, expect, 300))
 
-#     def test_full_vardecl(self):
-#         input = """x, y, z: integer = 1, 2, 3;"""
-#         expect = """Program([
-# 	VarDecl(x, IntegerType, IntegerLit(1))
-# 	VarDecl(y, IntegerType, IntegerLit(2))
-# 	VarDecl(z, IntegerType, IntegerLit(3))
-# ])"""
-#         self.assertTrue(TestAST.test(input, expect, 301))
+    def test_full_vardecl(self):
+        input = """x, y, z: integer = 1, 2, 3;"""
+        expect = """Program([
+	VarDecl(x, IntegerType, IntegerLit(1))
+	VarDecl(y, IntegerType, IntegerLit(2))
+	VarDecl(z, IntegerType, IntegerLit(3))
+])"""
+        self.assertTrue(TestAST.test(input, expect, 301))
 
-#     def test_vardecls(self):
-#         input = """x, y, z: integer = 1, 2, 3;
-#         a, b: float;"""
-#         expect = """Program([
-# 	VarDecl(x, IntegerType, IntegerLit(1))
-# 	VarDecl(y, IntegerType, IntegerLit(2))
-# 	VarDecl(z, IntegerType, IntegerLit(3))
-# 	VarDecl(a, FloatType)
-# 	VarDecl(b, FloatType)
-# ])"""
-#         self.assertTrue(TestAST.test(input, expect, 302))
+    def test_vardecls(self):
+        input = """x, y, z: integer = 1, 2, 3;
+        a, b: float;"""
+        expect = """Program([
+	VarDecl(x, IntegerType, IntegerLit(1))
+	VarDecl(y, IntegerType, IntegerLit(2))
+	VarDecl(z, IntegerType, IntegerLit(3))
+	VarDecl(a, FloatType)
+	VarDecl(b, FloatType)
+])"""
+        self.assertTrue(TestAST.test(input, expect, 302))
 
 #     def test_simple_program(self):
 #         """Simple program"""
